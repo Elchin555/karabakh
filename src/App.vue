0008e7a517b6741d3253f3c678cdebcd96c433dc
elchin.rahimli <elchinr@maestro.st>
2024-06-16 01:45:27 +0200

    recreate app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
-    <div class="md:pt-20 flex flex-col h-screen">
+    <Header v-if="showHeader" />
+    <div class="md:pt-20 flex flex-col h-screen ">
       <router-view class="" />
     </div>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+  computed: {
+    showHeader() {
+      return this.$route.meta.showHeader !== false;
+    },
+
+  },
+};
+</script>
